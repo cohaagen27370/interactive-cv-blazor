310d0e6904454abab7d65f26e482fa6535db1060
Laurent BLANQUET <46500674+cohaagen27370@users.noreply.github.com>
2025-06-14 18:29:49 +0200

    fix skill screen

--- Components/Skill/SkillContentModel.cs
+++ Components/Skill/SkillContentModel.cs
@@ -1,10 +1,24 @@
-﻿namespace interactiveCvBlazor.Components.Skill;
+﻿using Serilog;
+
+namespace interactiveCvBlazor.Components.Skill;
 
 public class SkillContentModel
 {
     public string Name { get; set; } = string.Empty;
 
     public int Level { get; set; }
+
+    public int LevelBy5
+    {
+        get
+        {
+            var propValue = (this.Level / 100.0) * 5.0;
+            var value = (int)propValue;
+            
+            Log.Debug(value.ToString());
+            return value == 0 ? 1 : value;;
+        }
+    } 
 
     public string Label { get; set; } = string.Empty;
     
